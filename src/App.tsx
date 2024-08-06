02f3b91af86b84cc83aa3ec0a74642e5e659143d
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-08-06 17:13:33 +0100

    feat: Dark mode switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { Box, Button, Flex, Heading, Input, ListItem, Stack, Text, UnorderedList } from "@chakra-ui/react"
+import { Box, Button, Flex, Heading, Input, ListItem, Stack, Text, UnorderedList, useColorMode, useColorModeValue } from "@chakra-ui/react"
 
 function App() {
+  const {colorMode,toggleColorMode} = useColorMode();
+  const containerForm = useColorModeValue("ButtonFace","#2b2b2b");
+  const bgForm = useColorModeValue("white","#333333");
 
   return (
-    <Flex bg={"ButtonFace"} h={"100vh"}>
-      <Stack spacing={3} w={"100%"} maxW={"md"} h={"fit-content"} bg={"white"} mx={"auto"} mt={"10%"} px={6} py={4} rounded={8} boxShadow={"lg"}>
+    <Flex bg={containerForm} h={"100vh"} position={"relative"}>
+      <Button onClick={toggleColorMode} position={"fixed"} top={2}right={2}>
+        {colorMode=="light" ? "Dark" :"Light"}
+      </Button>
+      <Stack spacing={3} w={"100%"} maxW={"md"} h={"fit-content"} bg={bgForm} mx={"auto"} mt={"10%"} px={6} py={4} rounded={8} boxShadow={"lg"}>
         <form action="">
           <Heading size={"lg"} my={6} fontSize={22} fontWeight={"500"}>Shopping List</Heading>
           <Flex gap={2}>
@@ -15,9 +21,9 @@ function App() {
         <Box maxH={300} overflow={"auto"}>
           <UnorderedList styleType={"none"} m={0} display={"flex"} flexDirection={'column'} gap={2} px={2} py={2}>
             {Array.from({length: 4}).map((_,index)=>(
-              <ListItem key={index} bg={"white"} border={"1px"} borderColor={"gray.200"} display={"flex"} alignItems={"center"} px={2} py={2.5} rounded={4}>
+              <ListItem key={index} border={"1px"} borderColor={"gray.200"} display={"flex"} alignItems={"center"} px={2} py={2.5} rounded={4}>
                 <Text flex={1}>I'm item {index+1}</Text>
-                <Button colorScheme="blackAlpha" size={"sm"} fontSize={13}>Delete</Button>
+                <Button colorScheme="gray" size={"sm"} fontSize={13}>Delete</Button>
               </ListItem>
             ))}
           </UnorderedList>
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
-
-const theme = extendTheme({
-  colors: {
-    brand: {
-      900: '#1a365d',
-      800: '#153e75',
-      700: '#2a69ac',
-    }
-  }
-})
+import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <App />
     </ChakraProvider> 
   </React.StrictMode>,
