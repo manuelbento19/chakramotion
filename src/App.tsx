ecacc8188b887848efcc0ceba0f241a77a18b4af
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-08-06 16:42:21 +0100

    feat: Chakra UI - Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
+import { Box, Button, Flex, Heading, Input, ListItem, Stack, Text, UnorderedList } from "@chakra-ui/react"
 
 function App() {
 
   return (
-    <section>
-      <article>
+    <Flex bg={"ButtonFace"} h={"100vh"}>
+      <Stack spacing={3} w={"100%"} maxW={"md"} h={"fit-content"} bg={"white"} mx={"auto"} mt={"10%"} px={6} py={4} rounded={8} boxShadow={"lg"}>
         <form action="">
-          <h1>Shopping List</h1>
-          <div className="">
-            <input type="text" placeholder='Add an item' />
-            <button>Add</button>
-          </div>
+          <Heading size={"lg"} my={6} fontSize={22} fontWeight={"500"}>Shopping List</Heading>
+          <Flex gap={2}>
+            <Input flex={1} placeholder='Add an item' />
+            <Button colorScheme="orange" fontSize={"1rem"}>Add</Button>
+          </Flex>
         </form>
-        <div className="">
-          <ul>
+        <Box maxH={300} overflow={"auto"}>
+          <UnorderedList styleType={"none"} m={0} display={"flex"} flexDirection={'column'} gap={2} px={2} py={2}>
             {Array.from({length: 4}).map((_,index)=>(
-              <li key={index}>
-                <span>I'm item {index+1} </span>
-                <button>Delete</button>
-              </li>
+              <ListItem key={index} bg={"white"} border={"1px"} borderColor={"gray.200"} display={"flex"} alignItems={"center"} px={2} py={2.5} rounded={4}>
+                <Text flex={1}>I'm item {index+1}</Text>
+                <Button colorScheme="blackAlpha" size={"sm"} fontSize={13}>Delete</Button>
+              </ListItem>
             ))}
-          </ul>
-        </div>
-      </article>
-    </section>
+          </UnorderedList>
+        </Box>
+      </Stack>
+    </Flex>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './index.css';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      900: '#1a365d',
+      800: '#153e75',
+      700: '#2a69ac',
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider> 
   </React.StrictMode>,
 )
