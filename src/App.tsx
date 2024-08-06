d6e89a3e79d9dd897ec9a24d1cbbc1a96469f97d
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-08-06 15:33:11 +0100

    feat: Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <section>
+      <article>
+        <form action="">
+          <h1>Shopping List</h1>
+          <div className="">
+            <input type="text" placeholder='Add an item' />
+            <button>Add</button>
+          </div>
+        </form>
+        <div className="">
+          <ul>
+            {Array.from({length: 4}).map((_,index)=>(
+              <li key={index}>
+                <span>I'm item {index+1} </span>
+                <button>Delete</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </article>
+    </section>
   )
 }
 
